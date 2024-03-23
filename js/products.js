8bf46c4635dfd645b5c537d3f7fa68253fda291d
Bryonpectol <46723917+Bryonpectol@users.noreply.github.com>
2024-03-23 00:00:47 -0600

    Update products.js

--- js/products.js
+++ js/products.js
@@ -2,75 +2,75 @@ const products = [
 
   {
     "name":"Drummer Short Sleeve Tee",
-    "price":"$19.98",
+    "price":"19.98",
     "link":"https://www.etsy.com/listing/1632481103/drummer-short-sleeve-tee",
     "image":"https://i.etsystatic.com/47668579/r/il/caa43c/5586293150/il_640xN.5586293150_2sib.jpg",
   },
   {
     "name":"Guitar Short Sleeve Tee",
-    "price":"$19.98",
+    "price":"19.98",
     "link":"https://www.etsy.com/listing/1632476013/guitar-short-sleeve-tee",
     "image":"https://i.etsystatic.com/47668579/r/il/1f67df/5586272750/il_640xN.5586272750_9zru.jpg",
   },
   {
     "name":"Evolution Guitar Short Sleeve Tee",
-    "price":"$19.98",
+    "price":"19.98",
     "link":"https://www.etsy.com/listing/1618301300/evolution-guitar-short-sleeve-tee",
     "image":"https://i.etsystatic.com/47668579/r/il/a79197/5586257542/il_640xN.5586257542_mb6x.jpg",
   },
   {
     "name":"I'll be in the basement drummer NuBlend® Crewneck Sweatshirt",
-    "price":"$24.98",
+    "price":"24.98",
     "link":"https://www.etsy.com/listing/1618295450/ill-be-in-the-basement-drummer-nublend",
     "image":"https://i.etsystatic.com/47668579/r/il/89f424/5634335217/il_640xN.5634335217_3f1u.jpg",
   },
   {
-    "name":"Drummer Short Sleeve Tee",
-    "price":"$19.98",
-    "link":"https://www.etsy.com/listing/1632481103/drummer-short-sleeve-tee",
-    "image":"https://i.etsystatic.com/47668579/r/il/caa43c/5586293150/il_640xN.5586293150_2sib.jpg",
+    "name":"May the Rhythm be with you NuBlend® Crewneck Sweatshirt",
+    "price":"24.98",
+    "link":"https://www.etsy.com/listing/1618293720/may-the-rhythm-be-with-you-nublend",
+    "image":"https://i.etsystatic.com/47668579/r/il/b19ef4/5586227630/il_640xN.5586227630_1hc2.jpg",
   },
   {
-    "name":"Drummer Short Sleeve Tee",
-    "price":"$19.98",
-    "link":"https://www.etsy.com/listing/1632481103/drummer-short-sleeve-tee",
-    "image":"https://i.etsystatic.com/47668579/r/il/caa43c/5586293150/il_640xN.5586293150_2sib.jpg",
+    "name":"Squatchin' through the snow Short Sleeve Tee",
+    "price":"19.98",
+    "link":"https://www.etsy.com/listing/1632460775/squatchin-through-the-snow-short-sleeve",
+    "image":"https://i.etsystatic.com/47668579/r/il/d89eed/5634314215/il_640xN.5634314215_2t9i.jpg",
   },
   {
-    "name":"Drummer Short Sleeve Tee",
-    "price":"$19.98",
-    "link":"https://www.etsy.com/listing/1632481103/drummer-short-sleeve-tee",
-    "image":"https://i.etsystatic.com/47668579/r/il/caa43c/5586293150/il_640xN.5586293150_2sib.jpg",
+    "name":"",
+    "price":"24.98",
+    "link":"https://www.etsy.com/listing/1618267812/hot-cocoa-vibes-nublend-crewneck",
+    "image":"https://i.etsystatic.com/47668579/r/il/2d6483/5586126402/il_640xN.5586126402_49p4.jpg",
   },
   {
-    "name":"Drummer Short Sleeve Tee",
-    "price":"$19.98",
-    "link":"https://www.etsy.com/listing/1632481103/drummer-short-sleeve-tee",
-    "image":"https://i.etsystatic.com/47668579/r/il/caa43c/5586293150/il_640xN.5586293150_2sib.jpg",
+    "name":"",
+    "price":"19.98",
+    "link":"",
+    "image":"",
   },
   {
-    "name":"Drummer Short Sleeve Tee",
-    "price":"$19.98",
-    "link":"https://www.etsy.com/listing/1632481103/drummer-short-sleeve-tee",
-    "image":"https://i.etsystatic.com/47668579/r/il/caa43c/5586293150/il_640xN.5586293150_2sib.jpg",
+    "name":"",
+    "price":"19.98",
+    "link":"",
+    "image":"",
   },
   {
-    "name":"Drummer Short Sleeve Tee",
-    "price":"$19.98",
-    "link":"https://www.etsy.com/listing/1632481103/drummer-short-sleeve-tee",
-    "image":"https://i.etsystatic.com/47668579/r/il/caa43c/5586293150/il_640xN.5586293150_2sib.jpg",
+    "name":"",
+    "price":"19.98",
+    "link":"",
+    "image":"",
   },
   {
-    "name":"Drummer Short Sleeve Tee",
-    "price":"$19.98",
-    "link":"https://www.etsy.com/listing/1632481103/drummer-short-sleeve-tee",
-    "image":"https://i.etsystatic.com/47668579/r/il/caa43c/5586293150/il_640xN.5586293150_2sib.jpg",
+    "name":"",
+    "price":"19.98",
+    "link":"",
+    "image":"",
   },
   {
-    "name":"Drummer Short Sleeve Tee",
-    "price":"$19.98",
-    "link":"https://www.etsy.com/listing/1632481103/drummer-short-sleeve-tee",
-    "image":"https://i.etsystatic.com/47668579/r/il/caa43c/5586293150/il_640xN.5586293150_2sib.jpg",
+    "name":"",
+    "price":"19.98",
+    "link":"",
+    "image":"",
   },
   
 ];
@@ -93,13 +93,16 @@ products.forEach(product => {
   priceText.className = "productname";
 
   nameText.textContent = "" + product.name;
-  priceText.textContent = "Price: " + product.price;
+  priceText.textContent = "Price: $" + product.price;
   Link.setAttribute("href", product.link);
   Link.setAttribute("target", "_blank");
   Link.textContent = "View Product";
   Image.setAttribute("src", product.image);
 
-  Container.appendChild(mainDiv);
+  if(product.name != "" || product.link != "" || product.image != "") {
+    Container.appendChild(mainDiv);
+  }
+  
   mainDiv.appendChild(nameText);
   mainDiv.appendChild(Image);
   mainDiv.appendChild(priceText);
