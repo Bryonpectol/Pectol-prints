568e4c81c894905528ba6b9308dbcfa00354d43d
Bryonpectol <46723917+Bryonpectol@users.noreply.github.com>
2024-03-22 23:27:47 -0600

    Update products.js

--- js/products.js
+++ js/products.js
@@ -1,1 +1,37 @@
+const products = [
 
+  {
+    "name":"Drummer Short Sleeve Tee",
+    "price":"$19.98",
+    "link":"https://www.etsy.com/listing/1632481103/drummer-short-sleeve-tee",
+    "image":"https://i.etsystatic.com/47668579/r/il/caa43c/5586293150/il_640xN.5586293150_2sib.jpg",
+  },
+  
+];
+
+
+let Container = document.getElementById("container");
+
+products.forEach(product => {
+
+  let mainDiv = document.createElement("div");
+  let nameText = document.createElement("p");
+  let priceText = document.createElement("p");
+  let Link = document.createElement("a");
+  let Image = document.createElement("img");
+
+  mainDiv.className = "productdiv";
+
+  nameText.textContent = "" + product.name;
+  priceText.textContent = "Price: " + product.price;
+  Link.setAttribute("href", product.link);
+  Link.textContent = "View Product";
+  Image.setAttribute("src", product.image);
+
+  Container.appendChild(mainDiv);
+  mainDiv.appendChild(nameText);
+  mainDiv.appendChild(Image);
+  mainDiv.appendChild(priceText);
+  mainDiv.appendChild(Link);
+  
+});
