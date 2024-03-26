e8ff7af46beea097f315ca5ae6e7129b5fe728d8
Bryonpectol <46723917+Bryonpectol@users.noreply.github.com>
2024-03-26 00:18:10 -0600

    Update productsdata.js

--- data/productsdata.js
+++ data/productsdata.js
@@ -99,7 +99,7 @@ export const products = [
     {
       "id":"",
       "name":"Pickleball Tee",
-      "price":"19.98",
+      "price":"24.98",
       "link":"https://www.etsy.com/listing/1604832902/pickleball-tee",
       "image":"https://i.etsystatic.com/47668579/r/il/2e9196/5586369527/il_794xN.5586369527_g23c.jpg",
       "category":"pickleball",
